132b021aa6b7c64dc22750e4eac85836408e8e71
Andrey Savelyev <andrey@undev.ru>
2013-07-09 12:38:49 +0400

    added method_missing

--- lib/hahashie/mash.rb
+++ lib/hahashie/mash.rb
@@ -1,15 +1,20 @@
 class Hahashie::Mash
-    attr_reader :hash
+  attr_reader :hash
 
-    def initialize hash = nil
-       @hash = hash
+  def initialize hash = nil
+   @hash = hash
 
-       @hash.each_pair do |key, value|
-            define_singleton_method "#{key}" do
-                value
-            end
-       end
+    if @hash 
+      @hash.each_pair do |key, value|
+        define_singleton_method "#{key}" do
+          value
+        end
+      end
     end
+  end
 
+  def method_missing(method)
+    return nil
+  end
 
 end
