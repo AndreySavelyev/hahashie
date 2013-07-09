6ebce3ddf2aa5ee63781b8540b64492f8f9a1bd7
Anatoliy Pronin <piton.pronin@gmail.com>
2013-07-09 17:19:45 +0400

    add hashes for Dash

--- lib/hahashie/dash.rb
+++ lib/hahashie/dash.rb
@@ -1,5 +1,20 @@
 class Hahashie::Dash
-  def initialize()
+  attr_reader :required_options
+  attr_reader :default_options
+  attr_reader :properties
 
+  def initialize(hash = nil)
+
+  end
+
+  class << self
+    def property(name, options = {})
+      properties << name
+      required_options << name if options[:required]
+
+      if options[:default]
+        default_options[name] = options[:default]
+      end
+    end
   end
 end
---
+++ spec/lib/dash_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Hahashie::Dash do
+  it "should be Dash object" do
+
+  end
+end
