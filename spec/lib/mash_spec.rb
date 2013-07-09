30e9fd63729d2750387ab5b4617d4f1dee1da37a
Andrey Savelyev <andrey@undev.ru>
2013-07-09 17:03:19 +0400

    new rspec syntax

--- spec/lib/mash_spec.rb
+++ spec/lib/mash_spec.rb
@@ -4,47 +4,48 @@ describe Hahashie::Mash do
   it "should be Mash object" do
     mash = Hahashie::Mash.new
     mash.should be
+    expect(mash).to be
   end
 
   it "should be Mash object's attribute" do
     mash = Hahashie::Mash.new
-    mash.name.should be_nil
+    expect(mash.name).to be_nil
   end
 
   it "should be Mash object's correct attribute" do
     mash = Hahashie::Mash.new({name: "test"})
-    mash.name.should eq "test"
+    expect(mash.name).to eq('test')
   end
 
   it 'check key existing' do
     mash = Hahashie::Mash.new({name: "test"})
-    mash.name?.should be_true
-    mash.age?.should be_false
+    expect(mash.name?).to be_true
+    expect(mash.age?).to be_false
   end
 
   it "check nested attribute" do
     mash = Hahashie::Mash.new({name: {one: "1", two: "2", three: {ololo: 'trololo'} }, age: '25'})
-    mash.name.is_a?(Hahashie::Mash).should be_true
+    expect(mash.name.class).to be Hahashie::Mash
   end
 
   it 'check assignment' do
     mash = Hahashie::Mash.new({name: "test"})
     mash.name = 'ololo'
-    mash.name.should eq 'ololo'
+    expect(mash.name).to eq('ololo')
   end
 
   it "check multi-level assigment" do
     mash = Hahashie::Mash.new
     mash.xer!.name = "123"
-    mash.xer.is_a?(Hahashie::Mash).should be_true
-    mash.xer.name.should eq "123"
+    expect(mash.xer.class).to be Hahashie::Mash
+    expect(mash.xer.name).to eq('123')
   end
 
   it 'check underscore access' do
     mash = Hahashie::Mash.new({a: 'aa', b: {c: 'cc'}} )
-    mash.a_.should eq 'aa'
-    mash.b_.c?.should be_true
-    mash.b_.should be_true
-    mash.b_.d?.should be_false
+    expect(mash.a_).to eq('aa')
+    expect(mash.b_.c?).to be_true
+    expect(mash.b_).to be_true
+    expect(mash.b_.d?).to be_false
   end
 end
