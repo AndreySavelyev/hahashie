debfe828cb70ca65ae4953f3924d26ead6d5af55
Andrey Savelyev <andrey@undev.ru>
2013-07-10 22:49:53 +0400

    clash working!!!

--- lib/hahashie/clash.rb
+++ lib/hahashie/clash.rb
@@ -1,34 +1,27 @@
 module Hahashie
   class Clash
     attr_reader :hash
-    attr_reader :tmp_hash
-    attr_reader :tmp_level
+    attr_reader :top
 
-    def initialize(hash = {})
+    def initialize(hash = {}, top = nil)
+      @top = top
       @hash = hash
-      @tmp_hash = {}
     end
 
     def method_missing(method, *args)
-      m_name = method[0...-1]
       case method[-1]
       when '!'
-        if "_".eql?(m_name[0])
-          self.send(@tmp_level, [@tmp_hash])
-          @tmp_level = nil
-          @tmp_hash = {}
-        else
-          @tmp_level = m_name
-        end
+        key = method[0...-1].to_sym
+        @hash[key] = Clash.new({}, self)
+        return @hash[key]
       else
-        if @tmp_level
-          @tmp_hash[method] = args.first
-        else
-          @hash[method] = args.first
-        end
+        @hash[method.to_sym] = args.first
       end
-      p "m_name = #{m_name}; method = #{method}; args = #{args}; self = #{self}"
       return self
+    end
+
+    def _end!
+      return @top
     end
 
     def [](name)
--- spec/lib/clash_spec.rb
+++ spec/lib/clash_spec.rb
@@ -4,7 +4,6 @@ describe Hahashie::Clash do
   it "should be Clash object" do
     clash = Hahashie::Clash.new
     clash.where(:abc => 'def').order(:created_at)
-
     expect(clash).to be
     expect(clash[:order]).to eq(:created_at)
     expect(clash[:where][:abc]).to eq('def')
@@ -12,12 +11,12 @@ describe Hahashie::Clash do
 
   it "should be Clash with bank" do
     clash = Hahashie::Clash.new
-    clash.where!.abc('def').ghi(123)._end!.order(:created_at)
-    p clash
-    p clash[:where]
-
+    clash.where!.abc('def').ghi(123)._end!.order(:created_at).zz('xx')
+    # clash.where!.abc('def').there!.ghi(123).zz('xx').yy('cc')._end!.order(:created_at)._end!.lala("ppp")
+    # {"where"=>{:abc=>"def", "there"=>{:ghi=>123, :zz=>"xx", :yy=>"cc"}, :order=>:created_at}, :lala=>"ppp"}
     expect(clash[:where][:abc]).to eq('def')
     expect(clash[:where][:ghi]).to eq(123)
     expect(clash[:order]).to eq(:created_at)
   end
+
 end
