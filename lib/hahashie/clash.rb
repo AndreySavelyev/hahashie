7222f1b71712cc6bb3ad3f59cea5933051d70711
Anatoliy Pronin <piton.pronin@gmail.com>
2013-07-10 17:21:54 +0400

    add Clashi

--- lib/hahashie/clash.rb
+++ lib/hahashie/clash.rb
@@ -1,20 +1,34 @@
 module Hahashie
   class Clash
     attr_reader :hash
+    attr_reader :tmp_hash
+    attr_reader :tmp_level
 
     def initialize(hash = {})
       @hash = hash
+      @tmp_hash = {}
     end
 
     def method_missing(method, *args)
       m_name = method[0...-1]
       case method[-1]
       when '!'
-
+        if "_".eql?(m_name[0])
+          self.send(@tmp_level, [@tmp_hash])
+          @tmp_level = nil
+          @tmp_hash = {}
+        else
+          @tmp_level = m_name
+        end
       else
-        @hash[method] = args.first
-        return self
+        if @tmp_level
+          @tmp_hash[method] = args.first
+        else
+          @hash[method] = args.first
+        end
       end
+      p "m_name = #{m_name}; method = #{method}; args = #{args}; self = #{self}"
+      return self
     end
 
     def [](name)
--- spec/lib/clash_spec.rb
+++ spec/lib/clash_spec.rb
@@ -5,10 +5,19 @@ describe Hahashie::Clash do
     clash = Hahashie::Clash.new
     clash.where(:abc => 'def').order(:created_at)
 
-    p clash
-
     expect(clash).to be
     expect(clash[:order]).to eq(:created_at)
     expect(clash[:where][:abc]).to eq('def')
+  end
+
+  it "should be Clash with bank" do
+    clash = Hahashie::Clash.new
+    clash.where!.abc('def').ghi(123)._end!.order(:created_at)
+    p clash
+    p clash[:where]
+
+    expect(clash[:where][:abc]).to eq('def')
+    expect(clash[:where][:ghi]).to eq(123)
+    expect(clash[:order]).to eq(:created_at)
   end
 end
