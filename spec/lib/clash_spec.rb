cbfcd8224cee8daaf3319b5746470780d00bcd96
Andrey Savelyev <andrey@undev.ru>
2013-07-10 22:52:31 +0400

    spec fxd

--- spec/lib/clash_spec.rb
+++ spec/lib/clash_spec.rb
@@ -11,12 +11,11 @@ describe Hahashie::Clash do
 
   it "should be Clash with bank" do
     clash = Hahashie::Clash.new
-    clash.where!.abc('def').ghi(123)._end!.order(:created_at).zz('xx')
-    # clash.where!.abc('def').there!.ghi(123).zz('xx').yy('cc')._end!.order(:created_at)._end!.lala("ppp")
+    clash.where!.abc('def').there!.ghi(123).zz('xx').yy('cc')._end!.order(:created_at)._end!.lala("ppp")
     # {"where"=>{:abc=>"def", "there"=>{:ghi=>123, :zz=>"xx", :yy=>"cc"}, :order=>:created_at}, :lala=>"ppp"}
+    expect(clash[:lala]).to eq('ppp')
     expect(clash[:where][:abc]).to eq('def')
-    expect(clash[:where][:ghi]).to eq(123)
-    expect(clash[:order]).to eq(:created_at)
+    expect(clash[:where][:there][:ghi]).to eq(123)
   end
 
 end
