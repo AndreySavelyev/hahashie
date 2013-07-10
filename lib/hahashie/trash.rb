eea25783bfcb3b011e6d82128ceeb4b94e862c5f
Anatoliy Pronin <piton.pronin@gmail.com>
2013-07-10 15:11:34 +0400

    add Trash tests

--- lib/hahashie/trash.rb
+++ lib/hahashie/trash.rb
@@ -5,10 +5,22 @@ module Hahashie
     end
 
     def self.property(name, options = {})
-      super(name. options)
+      super(name, options)
 
       @props_aliases ||= {}
-      @props_aliases[name] = options[:from] if options[:from]
+      @props_aliases[options[:from]] = name if options[:from]
+    end
+
+    def initialize(hash = {})
+      super
+
+      self.class.props_aliases.each_pair do |key, value|
+        @props_obj[value] = hash[key] ? hash[key] : hash[value]
+
+        define_setter key
+        define_setter value
+        define_getter value
+      end
     end
   end
 end
--- spec/lib/trash_spec.rb
+++ spec/lib/trash_spec.rb
@@ -2,17 +2,23 @@ require 'spec_helper'
 
 describe Hahashie::Trash do
   before do
-    class Person < Hahashie::Trash
+    class Person2 < Hahashie::Trash
+      property :original, from: :copy
       property :first_name, from: :firstName
     end
   end
 
   it "should be Trash object" do
-    @trash = Person.new(first_name: 'PName')
-    @trash2 = Person.new(firstName: 'Name2')
+    @trash = Person2.new(original: 'PName', firstName: "Men")
+    @trash2 = Person2.new(copy: 'Name2', first_name: "123")
+
+    s = "555"
+    @trash2.first_name = s
 
     expect(@trash).to be
-    expect(@trash.first_name).to eq('PName')
-    expect(@trash2.first_name).to eq('Name2')
+    expect(@trash.original).to eq('PName')
+    expect(@trash2.original).to eq('Name2')
+    expect(@trash.first_name).to eq("Men")
+    expect(@trash2.first_name).to eq(s)
   end
 end
